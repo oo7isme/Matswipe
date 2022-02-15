ee594e554d861d913ab224a6ab5a81ae1f202f61
Trygve <trygvemf@gmail.com>
2022-02-15 09:13:30 +0100

    Auth logout

--- JS/index.js
+++ JS/index.js
@@ -12,29 +12,43 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth()
 
+function signOut() {
+    let signOutButton = document.getElementById("signout")
+    signOutButton.addEventListener("click", (e) => {
+    //Prevent Default Form Submission Behavior
+    e.preventDefault()
+    console.log("clicked")
+    
+    auth.signOut()
+    alert("Signed Out")
+    window.location = "../index.html";
+})
+}
+
+
 //Signup Function
 let signUpButton = document.getElementById('signup')
 signUpButton.addEventListener("click", (e) => {
-//Prevent Default Form Submission Behavior
-e.preventDefault()
-console.log("clicked")
+    //Prevent Default Form Submission Behavior
+    e.preventDefault()
+    console.log("clicked")
 
-var email = document.getElementById("inputEmail")
-var password = document.getElementById("inputPassword")
-    
-auth.createUserWithEmailAndPassword(email.value, password.value)
-.then((userCredential) => {
-  location.reload();
-  // Signed in 
-  var user = userCredential.user;
-  console.log("user",user.email)
-})
-.catch((error) => {
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  console.log("error code", errorCode)
-  console.log("error Message", errorMessage)
-  });
+    var email = document.getElementById("inputEmail")
+    var password = document.getElementById("inputPassword")
+        
+    auth.createUserWithEmailAndPassword(email.value, password.value)
+    .then((userCredential) => {
+    location.reload();
+    // Signed in 
+    var user = userCredential.user;
+    console.log("user",user.email)
+    })
+    .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("error code", errorCode)
+    console.log("error Message", errorMessage)
+    });
 })
 
 
@@ -55,18 +69,18 @@ var password = document.getElementById("inputPassword")
 
 auth.signInWithEmailAndPassword(email.value, password.value) 
 .then((userCredential) => {
-  // location.reload();
-  // Signed in 
-  var user = userCredential.user;
-  console.log("user",user.email)
-  window.location = "PAGES/home.html";
-})
-.catch((error) => {
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // alert("error code", errorCode)
-  alert( errorMessage)
-  });
+    // location.reload();
+    // Signed in 
+    var user = userCredential.user;
+    console.log("user",user.email)
+    window.location = "PAGES/home.html";
+    })
+    .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // alert("error code", errorCode)
+    alert( errorMessage)
+    });
 })
 
 
@@ -89,7 +103,8 @@ auth.onAuthStateChanged(function(user) {
     users.appendChild(text);
 
     console.log(users)
-    //is signed in
+
+    window.location = "PAGES/home.html";
   } else {
     //no user signed in
   }
