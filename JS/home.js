0d356606ad7cafbcd2dd096a043951d59834d109
MasterDev16 <trygvemf@gmail.com>
2022-05-03 20:13:31 +0200

    error line 260

--- JS/home.js
+++ JS/home.js
@@ -19,7 +19,7 @@ const auth = getAuth(app);
 const db = getDatabase();
 
 let uid = '';
-onAuthStateChanged(auth, function(user) {
+onAuthStateChanged(auth, function (user) {
     if (user) {
         uid = user.uid
         console.log(uid);
@@ -60,11 +60,11 @@ function incrementIndex() {
 // MAIN SWIPE CODE
 var hammertime = new Hammer(currentCard);
 
-hammertime.on('pan', function(event) {
+hammertime.on('pan', function (event) {
     currentCard.classList.add('moving');
 });
 
-hammertime.on('pan', function(event) {
+hammertime.on('pan', function (event) {
     event.target.style.transition = '';
     if (event.deltaX === 0) return;
     if (event.center.x === 0 && event.center.y === 0) return;
@@ -79,7 +79,7 @@ hammertime.on('pan', function(event) {
     event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
 });
 
-hammertime.on('panend', function(event) {
+hammertime.on('panend', function (event) {
     event.target.style.transition = 'transition: all 0.3s ease-in-out';
     currentCard.classList.remove('moving');
     matswipeContainer.classList.remove('matswipe_love');
@@ -113,11 +113,11 @@ hammertime.on('panend', function(event) {
 
 // LIKE DISLIKE BUTTONS
 function createButtonListener(love) {
-    return function(event) {
+    return function (event) {
         document.getElementById("love").disabled = true;
         document.getElementById("nope").disabled = true;
-        setTimeout(function() { document.getElementById("love").disabled = false; }, 800);
-        setTimeout(function() { document.getElementById("nope").disabled = false; }, 800);
+        setTimeout(function () { document.getElementById("love").disabled = false; }, 800);
+        setTimeout(function () { document.getElementById("nope").disabled = false; }, 800);
 
         var moveOutWidth = document.body.clientWidth * 1.5;
         var card = document.getElementById('current_card')
@@ -151,7 +151,7 @@ var filter = get(child(dbRef, "filter/" + uid)).then((snapshot) => {
         console.log("Extracted data: ")
         console.log(filter)
     } else {
-        const writeFilterInit = async() => {
+        const writeFilterInit = async () => {
             try {
                 await set(ref(db, 'filter/' + uid), ["default"]);
                 location.reload()
@@ -227,7 +227,7 @@ let matretter = {
         { "id": "18", "title": "Bønnegryte med tomat", "desc": "Denne deilige bønnegryten har røtter i Hellas, der den gjerne bakes over åpen ild i flere timer. Vi har forenklet retten litt, men fortsatt er det viktig at bønnene får nok tid over varmen, så de blir møre og trekker til seg de gode smakene fra tomat, rosmarin og hvitløk", "pic": "https://images.matprat.no/73x8ft7bpd-jumbotron/large", "url": "https://www.matprat.no/oppskrifter/gjester/bonnegryte-med-tomat/", "tags": ["default", "tomat", "bønner", "løk", "hviløk"] },
         { "id": "19", "title": "Speilegg med bakt tomat", "desc": "Speilegg er enkel hverdagslykke. Sleng på noen bakte tomater for litt ekstra farge, som er til å bli ekstra glad av! ", "pic": "https://images.matprat.no/xu88t5jb2j-jumbotron/large", "url": "https://www.matprat.no/oppskrifter/sunn/speilegg-med-bakt-tomat/", "tags": ["default", "egg", "tomat", "brød", "olje"] },
         { "id": "20", "title": "Tomat og mozzarellasalat på glass", "desc": "En salat med friske tomater og fersk mozzarella er aldri feil! Spis tomat- og mozzarellasalaten som et lite mellommåltid, til lunsj eller server den som forrett til dine gjester. ", "pic": "https://images.matprat.no/tc47e5v5z2-jumbotron/large", "url": "https://www.matprat.no/oppskrifter/kos/tomat--og-mozzarellasalat-pa-glass/", "tags": ["default", "tomat", "basilikum", "olje"] },
-        { "id": "21", "title": "Kyllinggryte med kikerter, tomat og spinat", "desc": "Prøv denne superraske og enkle kyllinggryten med kikerter, hermetiske tomater og spinat. Dette er kjempegod og sunn hverdagskost!", "pic": "https://images.matprat.no/50d06d880d2f835f56001403-jumbotron/large", "url": "https://www.matprat.no/oppskrifter/sunn/kyllinggryte-med-kikerter-tomat-og-spinat/", "tags": ["default", "kylling", "spinat", "løk", "chili", "olje", ] }
+        { "id": "21", "title": "Kyllinggryte med kikerter, tomat og spinat", "desc": "Prøv denne superraske og enkle kyllinggryten med kikerter, hermetiske tomater og spinat. Dette er kjempegod og sunn hverdagskost!", "pic": "https://images.matprat.no/50d06d880d2f835f56001403-jumbotron/large", "url": "https://www.matprat.no/oppskrifter/sunn/kyllinggryte-med-kikerter-tomat-og-spinat/", "tags": ["default", "kylling", "spinat", "løk", "chili", "olje",] }
     ]
 };
 var obj;
@@ -239,7 +239,7 @@ function getMatretterListeFromDB() {
             console.log("Extracted data: ")
             console.log(obj)
         } else {
-            const writeListeData = async() => {
+            const writeListeData = async () => {
                 try {
                     await set(ref(db, 'users/' + uid), matretter);
                 } catch (ex) {
@@ -257,17 +257,9 @@ getMatretterListeFromDB()
 await new Promise(r => setTimeout(r, 500));
 
 if (obj.matretter != null) {
-    if (typeof(obj.matretter) == 'object') {
-        var matretterListe = Object.entries(obj.matretter).filter(matrett => {
-                console.log(matrett)
-                return matrett.tags.some(tag => filter.includes(tag))
-            })
-            // WORK HERE
-    } else {
-        var matretterListe = obj.matretter.filter(matrett => {
-            return matrett.tags.some(tag => filter.includes(tag))
-        })
-    }
+    var matretterListe = obj.matretter.filter(matrett => {
+        return matrett.tags.some(tag => filter.includes(tag))
+    })
 
 } else if (obj.matretter == null) {
     document.getElementById('show_nomorecards').style.display = 'block'
@@ -284,7 +276,7 @@ document.querySelector('#close').addEventListener('click', () => {
     document.getElementById('popup').style.display = 'none';
 
     const user = auth.currentUser;
-    const writeFilterData = async() => {
+    const writeFilterData = async () => {
         try {
             await set(ref(db, 'filter/' + uid), filter);
         } catch (ex) {
@@ -448,33 +440,20 @@ onValue(ref(db, 'users/' + uid + '/liked'), (snapshot) => {
             if (data == null) {
                 return
             } else {
-                if (typeof(data) == 'object') {
-                    Object.entries(data).forEach((data) => {
-                        if (data.id == matrett.id) {
-                            const removeFromList = async() => {
-                                try {
-                                    await set(ref(db, 'users/' + uid + "/matretter/" + data.id), null);
-                                } catch (ex) {
-                                    console.error(`Error while setting data: ${ex.message}`);
-                                }
-                            };
-                            removeFromList();
-                        }
-                    })
-                } else {
-                    data.forEach((data) => {
-                        if (data.id == matrett.id) {
-                            const removeFromList = async() => {
-                                try {
-                                    await set(ref(db, 'users/' + uid + "/matretter/" + data.id), null);
-                                } catch (ex) {
-                                    console.error(`Error while setting data: ${ex.message}`);
-                                }
-                            };
-                            removeFromList();
-                        }
-                    })
-                }
+                console.log('obj')
+                Object.entries(data).forEach((data) => {
+                    if (data[1].id == matrett.id) {
+                        const removeFromList = async () => {
+                            try {
+                                await set(ref(db, 'users/' + uid + "/matretter/" + data[1].id), null);
+                                console.log('removed obj')
+                            } catch (ex) {
+                                console.error(`Error while setting data: ${ex.message}`);
+                            }
+                        };
+                        removeFromList();
+                    }
+                })
 
             }
 
@@ -490,10 +469,10 @@ onValue(ref(db, 'users/' + uid + '/disliked'), (snapshot) => {
             if (data == null) {
                 return
             } else {
-                if (typeof(data) == 'object') {
+                if (typeof (data) == 'object') {
                     Object.entries(data).forEach((data) => {
                         if (data.id == matrett.id) {
-                            const removeFromList = async() => {
+                            const removeFromList = async () => {
                                 try {
                                     await set(ref(db, 'users/' + uid + "/matretter/" + data.id), null);
                                 } catch (ex) {
@@ -506,7 +485,7 @@ onValue(ref(db, 'users/' + uid + '/disliked'), (snapshot) => {
                 } else {
                     data.forEach((data) => {
                         if (data.id == matrett.id) {
-                            const removeFromList = async() => {
+                            const removeFromList = async () => {
                                 try {
                                     await set(ref(db, 'users/' + uid + "/matretter/" + data.id), null);
                                 } catch (ex) {
